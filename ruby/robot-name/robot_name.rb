14663b93aaf9b4a2af112c2f68ea0f6c518fe5d8
David Elliott <david@philosophie.is>
2016-08-16 16:17:07 -0700

    robot names passing

---
+++ ruby/robot-name/robot_name.rb
@@ -0,0 +1,35 @@
+module BookKeeping
+  VERSION = 2
+end
+
+class Robot
+  def name
+    @name ||= begin
+      name = generate_name until Robot.name_available? name
+      Robot.register_name name
+      name
+    end
+  end
+
+  def reset
+    @name = nil
+  end
+
+  private
+
+  def generate_name
+    a = ('A'..'Z').to_a
+    n = (1..9).to_a.map(&:to_s)
+    a[rand(26)] + a[rand(26)] + n[rand(9)] + n[rand(9)] + n[rand(9)]
+  end
+
+  def self.register_name(name)
+    @@names << name
+  end
+
+  def self.name_available?(name)
+    return false if name.to_s.empty?
+    @@names ||= []
+    !@@names.include? name
+  end
+end
