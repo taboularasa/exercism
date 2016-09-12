0fe4e5a6a30db879d0489baa818a347cbe560ca3
David Elliott <david@hadto.net>
2016-09-12 16:19:27 -0700

    make leap pass

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,12 +1,19 @@
 // Leap stub file
 
-// The package name is expected by the test program.
 package leap
 
 // testVersion should match the targetTestVersion in the test file.
 const testVersion = 2
 
-// It's good style to write a comment here documenting IsLeapYear.
-func IsLeapYear(int) bool {
-	// Write some code here to pass the test suite.
+func IsLeapYear(year int) bool {
+	switch {
+	case year%400 == 0:
+		return true
+	case year%100 == 0:
+		return false
+	case year%4 == 0:
+		return true
+	default:
+		return false
+	}
 }
