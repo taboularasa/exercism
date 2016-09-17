dc59f5b4fe79e30328432742b72135e634e8d7aa
David Elliott <david@hadto.net>
2016-09-17 16:18:47 -0700

    make hamming pass

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,7 +1,24 @@
 package hamming
 
+import "fmt"
+
 const testVersion = 4
 
+//Distance takes two strings and returns the count of positional differences
 func Distance(a, b string) (int, error) {
-	// your code here!
+	aSlice := []rune(a)
+	bSlice := []rune(b)
+	distance := 0
+
+	if len(aSlice) != len(bSlice) {
+		return -1, fmt.Errorf("a and b must be of equal length")
+	}
+
+	for i := range aSlice {
+		if aSlice[i] != bSlice[i] {
+			distance++
+		}
+	}
+
+	return distance, nil
 }
