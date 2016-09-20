64a8c711bea61c46c63677fd0de3ee8c51c8abc1
David Elliott <david@hadto.net>
2016-09-20 16:48:07 -0700

    WIP working on go bob

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -1,3 +1,17 @@
 package bob // package name must match the package name in bob_test.go
+import "strings"
 
 const testVersion = 2 // same as targetTestVersion
+
+// Hey returns a sassy string dispached by an input string
+func Hey(input string) string {
+	if strings.ToUpper(input) == input {
+		return "Whoa, chill out!"
+	}
+
+	if strings.HasSuffix(input, "?") {
+		return "Sure."
+	}
+
+	return "Whatever."
+}
