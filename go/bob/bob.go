b34e4534ff35ae80ddb5af5b9bdd225b179c8b2e
David Elliott <david.elliott@stitchfix.com>
2017-01-26 20:18:26 -0800

    Make some more bob tests pass

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -1,11 +1,22 @@
 package bob // package name must match the package name in bob_test.go
-import "strings"
+import (
+	"strconv"
+	"strings"
+)
 
 const testVersion = 2 // same as targetTestVersion
 
 // Hey returns a sassy string dispached by an input string
 func Hey(input string) string {
 	if strings.ToUpper(input) == input {
+		t := strings.Split(input, ",")
+		for _, i := range t {
+			_, err := strconv.Atoi(i)
+			if err != nil {
+				return "Whatever."
+			}
+		}
+
 		return "Whoa, chill out!"
 	}
 
